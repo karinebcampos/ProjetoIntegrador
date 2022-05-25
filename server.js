44de3a93b0ade0cbcbe3c1867eaf20b6f444e22d
Karine Campos <kaarine_campos@hotmail.com>
2022-05-25 20:11:46 -0300

    testando back-end

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const { sequelize } = require('./db');
-const { usuario } = require('./back-end/usuario.js');
+const { Usuario } = require('./back-end/usuario.js');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.post('/register', async (req,res) => {
+    console.log(req.body);
+    const usuario = new Usuario();
+    usuario.nome = req.body.name;
+    usuario.email = req.body.email;
+    usuario.senha = req.body.password;
+    await usuario.save();
+    res.json(usuario);
+})
 
 app.listen(3000, async () => {
     await sequelize.sync();
